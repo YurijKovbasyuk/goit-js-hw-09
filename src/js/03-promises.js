865474e062c8b5bec036b3fd1a2a56ada810f332
YurijKovbasyuk <kovbasyuk.yurij@gmail.com>
2022-05-15 20:40:54 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -9,29 +9,24 @@ const refs = {
 
 refs.createButton.addEventListener('click', onHandleClick);
 
-let delay = refs.step.value;
-let amount = refs.amount.value;
-let firstDelay = refs.delay.value;
+const delay = 1500;
 function onHandleClick(e, delay, firstDelay, amount) {
   e.preventDefault();
-
-  setTimeout(createPromise(), firstDelay)
-    .then((position, delay) => {
-      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    })
-    .catch((position, delay) => {
-      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    });
-
-  for (let position = 1; i <= amount; i++) {
-    let delay = 
-    createPromise(position, delay)
-      .then((position, delay) => {
-        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      })
-      .catch((position, delay) => {
-        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-      });
+  let position;
+  for (let position = 1; position <= refs.amount.value; position++) {
+    setTimeout(() => {
+      createPromise(position, delay)
+        .then((position, delay) => {
+          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch((position, delay) => {
+          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        })
+        .finally((position, delay) => {
+          position += 1;
+          // delay = delay + 1000;
+        });
+    }, refs.delay.value);
   }
 }
 function createPromise(position, delay) {
@@ -40,11 +35,13 @@ function createPromise(position, delay) {
     setTimeout(() => {
       if (shouldResolve) {
         // Fulfill
-        resolve('done');
+
+        resolve(`${position}`);
       } else {
         // Reject
-        reject('not');
+
+        reject(`${position}`);
       }
-    });
+    }, refs.firstDelay.value);
   });
 }
