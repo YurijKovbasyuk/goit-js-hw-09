010a692367c3710dc1ba41351ac7dc3fefca3469
YurijKovbasyuk <kovbasyuk.yurij@gmail.com>
2022-05-15 21:22:17 +0300

    Update 03-promises.js
    Usage Notiflix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,4 @@
-// import { Loading } from 'notiflix';
+import Notiflix from 'notiflix';
 
 const refs = {
   createButton: document.querySelector('button'),
@@ -9,25 +9,26 @@ const refs = {
 
 refs.createButton.addEventListener('click', onHandleClick);
 
-const delay = 1500;
 function onHandleClick(e, delay, firstDelay, amount) {
   e.preventDefault();
-  let position;
-  for (let position = 1; position <= refs.amount.value; position++) {
-    setTimeout(() => {
+
+  setTimeout(() => {
+    for (let position = 1; position <= refs.amount.value; position++) {
+      
       createPromise(position, delay)
         .then((position, delay) => {
+          Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
           console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
         })
         .catch((position, delay) => {
+          Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
           console.log(`❌ Rejected promise ${position} in ${delay}ms`);
         })
         .finally((position, delay) => {
           position += 1;
-          // delay = delay + 1000;
         });
-    }, refs.delay.value);
-  }
+    }
+  }, refs.delay.value);
 }
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
